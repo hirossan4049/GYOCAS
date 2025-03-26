185c0019d7b9ea93c479593f38853e39c9d2f06a
hirossan4049 <50548952+hirossan4049@users.noreply.github.com>
2025-03-26 14:40:44 +0000

    black to ruff

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env -S uv run -s
 import os
+import signal
 import subprocess
 import time
-
-import signal
 import zoneinfo
 from datetime import datetime
-from dotenv import load_dotenv
 
 import boto3
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -20,47 +19,47 @@ S3_DIR = os.environ.get("S3_DIR")
 USER_ID = os.environ.get("USER_ID")
 
 wasabi = boto3.client(
-    "s3",
-    endpoint_url=f"https://s3.{REGION_NAME}.wasabisys.com",
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_ACCESS_KEY,
+  "s3",
+  endpoint_url=f"https://s3.{REGION_NAME}.wasabisys.com",
+  aws_access_key_id=ACCESS_KEY,
+  aws_secret_access_key=SECRET_ACCESS_KEY,
 )
 
 
 def download():
-    return subprocess.Popen(
-        [
-            "ffmpeg",
-            "-y",
-            "-i",
-            f"http://twitcasting.tv/{USER_ID}/metastream.m3u8/?video=1",
-            "-c",
-            "copy",
-            "-map",
-            "p:0",
-            filename,
-        ]
-    )
+  return subprocess.Popen(
+    [
+      "ffmpeg",
+      "-y",
+      "-i",
+      f"http://twitcasting.tv/{USER_ID}/metastream.m3u8/?video=1",
+      "-c",
+      "copy",
+      "-map",
+      "p:0",
+      filename,
+    ]
+  )
 
 
 while True:
-    print("download.sh を起動します。")
-    date = datetime.now(zoneinfo.ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d_%H-%M")
-    filename = f"{USER_ID}_{date}.mp4"
+  print("download.sh を起動します。")
+  date = datetime.now(zoneinfo.ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d_%H-%M")
+  filename = f"{USER_ID}_{date}.mp4"
+  process = download()
+  print(f"PID: {process.pid}")
+
+  time.sleep(5)
+  isLive = False
+  if process.poll() is None:
+    isLive = True
+    print("配信始めたぞあいつ")
+    time.sleep(20)
+    process.send_signal(signal.SIGUSR1)
     process = download()
-    print(f"PID: {process.pid}")
 
-    time.sleep(5)
-    isLive = False
-    if process.poll() is None:
-        isLive = True
-        print("配信始めたぞあいつ")
-        time.sleep(10)
-        process.send_signal(signal.SIGUSR1)
-        process = download()
+  process.wait()
+  if isLive:
+    wasabi.upload_file(filename, S3_BUCKET, f"{USER_ID}/{filename}")
 
-    process.wait()
-    if isLive:
-        wasabi.upload_file(filename, S3_BUCKET, f"{USER_ID}/{filename}")
-
-    time.sleep(5)
+  time.sleep(5)
